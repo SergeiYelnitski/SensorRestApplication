aafd50317e62dab0cd02ef2e2f69b088c7121540
Sergei Yelnitski <sergei.yelnitski@gmail.com>
2022-08-28 21:32:53 +0300

    create

---
+++ SensorRestServer/src/main/java/by/yelnitski/sensor/rest/server/SensorRestServerApplication.java
@@ -0,0 +1,13 @@
+package by.yelnitski.sensor.rest.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorRestServerApplication.class, args);
+    }
+
+}
---
+++ SensorRestServer/src/test/java/by/yelnitski/sensor/rest/server/SensorRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yelnitski.sensor.rest.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
